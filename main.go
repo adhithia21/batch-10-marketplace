e9fa176f1168bd66fa60a6369d4c5d894ba8eb69
Adhithia Irvan <adhithia.irvan@gmail.com>
2024-02-10 11:12:55 +0700

    update json key version 1.0

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
+			"version": "v1.0",
 		})
 	})
 	r.Run(":8000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
